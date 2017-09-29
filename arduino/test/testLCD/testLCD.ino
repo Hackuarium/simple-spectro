0cbad56065d21d3fb5bc9df5f63c9af024c219ef
lpatiny <gb252243>
2017-09-29 19:11:24 +0200

    improve testLCD

--- arduino/test/testLCD/testLCD.ino
+++ arduino/test/testLCD/testLCD.ino
@@ -1,5 +1,3 @@
-#include <LiquidCrystal.h>
-
 #define LCD_E   12
 #define LCD_RS  A6
 #define LCD_D4  6
@@ -12,40 +10,31 @@
 
 byte pins[] = {LCD_E, LCD_RS, LCD_D4, LCD_D5, LCD_D6, LCD_D7, LCD_VO};
 
-// initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
-
-void setup() {
-  // testPins(); // just to test connections with oscilloscope
-  pinMode(LCD_BL, OUTPUT);
-  digitalWrite(LCD_BL, HIGH); // backlight
-  pinMode(LCD_ON, HIGH); // LCD on / off
-  digitalWrite(LCD_ON, HIGH); // backlight
-  pinMode(13, INPUT); // contrast control / turned off in this version
-  delay(10);
-  lcd.begin(16, 2);
-  lcd.print("hello, world!");
-}
+void setup() {}
 
 void loop() {
-  lcd.setCursor(0, 1);
-  lcd.print(millis() / 1000);
+  testPins(); // just to test connections with oscilloscope
 }
 
 
 void testPins() {
+  long counter = 0;
   for (byte i = 0; i < sizeof(pins); i++) {
     pinMode(pins[i], OUTPUT);
   }
+
   while (true) {
     for (byte i = 0; i < sizeof(pins); i++) {
-      digitalWrite(pins[i], HIGH);
+      if (counter % (i + 1) == 0) {
+        if (digitalRead(pins[i]) == HIGH) {
+          digitalWrite(pins[i], LOW);
+        } else {
+          digitalWrite(pins[i], HIGH);
+        }
+      }
     }
-    delay(10);
-    for (byte i = 0; i < sizeof(pins); i++) {
-      digitalWrite(pins[i], LOW);
-    }
-    delay(10);
+    counter++;
+    delay(1);
   }
 }
 
