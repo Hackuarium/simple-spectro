48e58e32d645072bb5b4a8e244b475797e40b938
Luc Patiny <lpatiny@users.noreply.github.com>
2017-10-02 00:04:24 +0200

    Update Z_Start_thread.ino

--- arduino/SimpleSpectro/Z_Start_thread.ino
+++ arduino/SimpleSpectro/Z_Start_thread.ino
@@ -1,10 +1,11 @@
 NIL_THREADS_TABLE_BEGIN()
 
+  // the count should be exact, Acquisition has the higher priority ...
+NIL_THREADS_TABLE_ENTRY(NULL, ThreadAcquisition, NULL, waThreadAcquisition, sizeof(waThreadAcquisition))
+
 NIL_THREADS_TABLE_ENTRY(NULL, ThreadSerial, NULL, waThreadSerial, sizeof(waThreadSerial))
 
 NIL_THREADS_TABLE_ENTRY(NULL, ThreadLcd, NULL, waThreadLcd, sizeof(waThreadLcd))
-
-NIL_THREADS_TABLE_ENTRY(NULL, ThreadAcquisition, NULL, waThreadAcquisition, sizeof(waThreadAcquisition))
 
 NIL_THREADS_TABLE_END()
 
