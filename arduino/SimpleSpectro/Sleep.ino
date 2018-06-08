1f690b39cecf741ab7c045f7d3324e53894cf45d
Luc Patiny <luc@patiny.com>
2018-06-08 22:22:24 +0200

    fix sleep mode

--- arduino/SimpleSpectro/Sleep.ino
+++ arduino/SimpleSpectro/Sleep.ino
@@ -9,13 +9,13 @@ void sleepNow () {
   set_sleep_mode (SLEEP_MODE_PWR_DOWN);
 
   nilThdSleepMilliseconds(2);  // smll debouncing
-  attachInterrupt (digitalPinToInterrupt (ROT_B), empty, CHANGE);
+ // attachInterrupt (digitalPinToInterrupt (ROT_B), empty, CHANGE);
 
   interrupts ();           // interrupts allowed now, next instruction WILL be executed
   sleep_cpu ();            // here the device is put to sleep
 
   sleep_disable ();         // first thing after waking from sleep:
-  detachInterrupt (digitalPinToInterrupt (ROT_B));      // stop LOW interrupt on D2
+ // detachInterrupt (digitalPinToInterrupt (ROT_B));      // stop LOW interrupt on D2
   wdt_enable(WDTO_8S);  //reactivate the watchdog
   wakeUpScreen();
 }
