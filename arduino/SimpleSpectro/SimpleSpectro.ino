210e285894577e3f9e0ee67b97504efda7419c23
Luc Patiny <luc@patiny.com>
2019-03-16 18:52:38 +0100

    update libino with WireM

--- arduino/SimpleSpectro/SimpleSpectro.ino
+++ arduino/SimpleSpectro/SimpleSpectro.ino
@@ -6,6 +6,7 @@
 // version B : temperature sensor and battery indicator
 // version C : same as B (version 1.4)
 // version D : need to enable LCD (pin 13) and temperature / light sensor: PE2
+// version E : introduce I2C component for led control (version 2.0)
 
 #define VERSION   D
 
@@ -16,13 +17,13 @@
 #define BLUE   A2
 #define UV1    A4 // 5mm
 
-#if VERSION == B || VERSION == C || VERSION == D
+#if VERSION == B || VERSION == C || VERSION == D || VERSION == E
 #define BATTERY               A3  // if battery we have also the temperature sensor
 
 #define TEMPERATURE_ADDRESS   0b1001000
 #endif
 
-#if VERSION == D
+#if VERSION == D || VERSION == E
 #define POWER_ON_DSL237       PORTE |= 1 << PE2; DDRE |= 1 << PE2; nilThdSleepMilliseconds(10);
 #define POWER_OFF_DSL237      PORTE &= ~ (1 << PE2);
 #define BATTERY_CHARGING      A5
@@ -33,7 +34,7 @@
 #define BATTERY_LEVEL 128   // not available in version A
 #define TEMPERATURE   129   // not available in version A
 
-#if VERSION == 'A'
+#if VERSION == A
 byte ALL_PARAMETERS[] = {RED, GREEN, BLUE, UV1};  // all possible leds
 byte CURRENT_PARAMETERS[] = {RED, GREEN, BLUE, UV1};      // will contain the active les
 #else
@@ -47,7 +48,23 @@ byte dataRowSize;         // size of a data row (number of entries in data)
 byte maxNbRows;           // calculate value depending the size of EEPROM dedicated to logs
 
 #define THR_SERIAL      1
-#define I2C_HARDWARE    0
+#define I2C_HARDWARE    1
+
+#define I2C_TIMEOUT 10
+#if VERSION == E 
+  #define SDA_PIN 7
+  #define SDA_PORT PORTF
+  #define SCL_PIN 6
+  #define SCL_PORT PORTF
+  #define I2C_FASTMODE 0
+#else
+  #define SDA_PIN 1
+  #define SDA_PORT PORTD
+  #define SCL_PIN 0
+  #define SCL_PORT PORTD
+  #define I2C_FASTMODE 1
+#endif
+
 #define THR_WIRE_MASTER 1
 #define MAX_PARAM       26
 
@@ -57,8 +74,11 @@ void setup() {
   setupParameters();
   checkParameters(); // setup automatically the default parameter after install boot loader
   setActiveLeds();
+ // setupMCP4828();
   nilSysBegin();
 }
 
-void loop() {}
+void loop() {
+ // loopMCP4828();
+  }
 
